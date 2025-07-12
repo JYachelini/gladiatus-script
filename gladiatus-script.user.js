86027291939487b6e5c9cbfebb8d8c6c6d4dd333
JYachelini <juliyachedev@gmail.com>
2025-07-12 01:53:12 -0300

    v 1.8

--- gladiatus-script.user.js
+++ gladiatus-script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Gladiatus Script - JYachelini version
-// @version      1.7
+// @version      1.8
 // @description  Gladiatus Script
 // @author       JYachelini
 // @match        *://*.gladiatus.gameforge.com/game/index.php*
