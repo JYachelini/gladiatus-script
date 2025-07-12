724ab18671fab4dc42af5bd2128b3cc95e7c7a1a
JYachelini <juliyachedev@gmail.com>
2025-07-12 02:21:56 -0300

    fix

--- gladiatus-script.user.js
+++ gladiatus-script.user.js
@@ -946,7 +946,7 @@
             break;
         }
       });
-      localStorage.setItem("trainingCosts", JSON.stringify(trainingCosts));
+      localStorage.setItem("currentTraining", JSON.stringify(currentTraining));
     }
   }
 
